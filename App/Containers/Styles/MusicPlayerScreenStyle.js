c721e3f19f62b24769e24e7b751926b50f59df53
Toni Ristola <toni.ristola@gofore.com>
2017-04-08 08:57:20 +0300

    removed rounded corners

--- App/Containers/Styles/MusicPlayerScreenStyle.js
+++ App/Containers/Styles/MusicPlayerScreenStyle.js
@@ -1,11 +1,10 @@
 import { StyleSheet } from 'react-native'
-import { ApplicationStyles, Colors, Metrics, Fonts } from '../../Themes/'
+import { ApplicationStyles, Colors, Fonts } from '../../Themes/'
 
 export default StyleSheet.create({
   ...ApplicationStyles.screen,
   container: {
     flex: 1,
-    paddingTop: Metrics.doubleBaseMargin,
     backgroundColor: 'rgba(0, 0, 0, 0.8)'
   },
   text: {
--- App/Containers/Styles/SongListStyle.js
+++ App/Containers/Styles/SongListStyle.js
@@ -4,13 +4,8 @@ import { Colors, Metrics } from '../../Themes/'
 export default StyleSheet.create({
   container: {
     flex: 1,
-    borderTopLeftRadius: Metrics.doubleBaseMargin,
-    borderTopRightRadius: Metrics.doubleBaseMargin,
     paddingLeft: 40,
     paddingTop: Metrics.doubleBaseMargin,
-    marginLeft: Metrics.doubleBaseMargin,
-    marginRight: Metrics.doubleBaseMargin,
-    paddingBottom: Metrics.baseMargin,
     backgroundColor: 'rgba(0, 0, 0, 0.5)'
   },
   text: {
